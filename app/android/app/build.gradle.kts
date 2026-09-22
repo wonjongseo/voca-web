cc6f443f1c9f0f7dc9956d80f3df769714f90740
Jongseo Won <visionwill@naver.com>
2026-09-22 16:36:57 +0900

    delete files

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.leafy"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
